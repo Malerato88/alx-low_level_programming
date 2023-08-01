178d3deafb1a69d120b1c61946840babc6b4c0f0
Malerato88 <phathu2.maradwa@gmail.com>
2023-08-01 16:00:10 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,10 +16,10 @@ char *_strpbrk(char *s, char *accept)
 	{
 		for (i = 0; accept[i]; i++)
 		{
-			if (*s == accept [i])
+			if (*s == accept[i])
 				return (s);
 		}
 		s++;
 	}
-	return('\0');
+	return ('\0');
 }
